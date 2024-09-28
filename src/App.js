6557d015d4e37a4625ba0a9167b04ee5d8e7939c
Lakshay Dhiman <lakshay@acmeminds.com>
2024-09-28 17:20:53 +0530

    Updated the URL and put it into the config

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './components/Header';
 import Menu from './components/Menu';
+import config from './config';
 
 const App = () => {
     const [menuItems, setMenuItems] = useState([]);  // Default to an empty array
     const [loading, setLoading] = useState(true);    // Add loading state
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/menu')
+        axios.get(`${config.API_URL}/api/menu`)
             .then(response => {
                 if (response.data && response.data.items) {
                     setMenuItems(response.data.items);  // Access the items array
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    // API_URL: 'http://localhost:5000', //test
+    API_URL: 'https://tech-tiffin-api.onrender.com', //prod
+};
+
+export default config;
